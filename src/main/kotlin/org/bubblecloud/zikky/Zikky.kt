87118392d922b8b7b235050d50f122d970a1a374
Tommi S.E. Laukkanen <tommi.s.e.laukkanen@gmail.com>
2016-07-04 20:21:14 +0300

    Experimenting with Kotlin and zigbee4java.

--- src/main/kotlin/org/bubblecloud/zikky/Zikky.kt
+++ src/main/kotlin/org/bubblecloud/zikky/Zikky.kt
@@ -1,10 +1,78 @@
 package org.bubblecloud.zikky
 
-
-fun getHelloString() : String {
-    return "Hello, world!"
-}
+import org.bubblecloud.zigbee.v3.ZigBeeGatewayClient
+import org.bubblecloud.zigbee.v3.zcl.protocol.ZclClusterType
+import org.bubblecloud.zigbee.v3.zcl.protocol.command.general.ReportAttributesCommand
+import org.bubblecloud.zigbee.v3.zcl.protocol.command.ias.zone.ZoneStatusChangeNotificationCommand
 
 fun main(args : Array<String>) {
-    println(getHelloString())
+    println("Zikky startup...")
+    val api = ZigBeeGatewayClient("http://127.0.0.1:5000/", "secret")
+    api.startup()
+    println("Zikky startup.")
+
+    onExit({
+        println()
+        println("Zikky shutdown...")
+        api.shutdown()
+        println("Zikky shutdown.")
+    })
+
+    var devices = api.zigBeeDevices.filter { it.label != null }.associateBy({it.label}, {it})
+
+    println()
+    println("Labeled devices in network:")
+
+    for ((index, device) in devices.values.withIndex()) {
+        val label: String
+        if (device.label != null) {
+            label = device.label
+        } else {
+            label = "<no label>"
+        }
+        print(index.toString().padStart(3))
+        print(") ")
+        print(device.networkAddress.toString().padEnd(6))
+        print(": ")
+        print(label)
+        println()
+    }
+
+    api.addCommandListener {
+        if (it is ZoneStatusChangeNotificationCommand) {
+            val command: ZoneStatusChangeNotificationCommand = it
+            val alarm1Mask = 1 shl 0
+            val alarm2Mask = 1 shl 1
+            val alarm1 = command.zoneStatus and alarm1Mask > 0
+            val alarm2 = command.zoneStatus and alarm2Mask > 0
+            println("Zone status alarm 1: $alarm1 alarm 2: $alarm2")
+        }
+        if (it is ReportAttributesCommand) {
+            val command: ReportAttributesCommand = it
+            if (command.clusterId == ZclClusterType.OCCUPANCY_SENSING.id) {
+                for (report in command.reports) {
+                    println("Occupancy and sensing reports: ${report.attributeIdentifier} = ${report.attributeValue}")
+                }
+            }
+
+        }
+    }
+
+    Thread.sleep(10000)
+
+    val lampOne = devices["l1"]
+
+    if (lampOne != null) {
+        api.off(lampOne)
+    }
+
+    exit()
+}
+
+fun exit() {
+    Runtime.getRuntime().exit(0)
+}
+
+fun onExit(shutdownHook: () -> Unit) {
+    Runtime.getRuntime().addShutdownHook(Thread(shutdownHook))
 }
