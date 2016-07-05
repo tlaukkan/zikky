d86dd21f31212e034a1e135a1e34c0306b135767
Tommi S.E. Laukkanen <tommi.s.e.laukkanen@gmail.com>
2016-07-05 20:09:32 +0300

    Refactored security zone alarm based light group on / off switching.

--- src/main/kotlin/org/bubblecloud/zikky/Zikky.kt
+++ src/main/kotlin/org/bubblecloud/zikky/Zikky.kt
@@ -1,84 +1,72 @@
 package org.bubblecloud.zikky
 
 import org.bubblecloud.zigbee.v3.ZigBeeGatewayClient
-import org.bubblecloud.zigbee.v3.ZigBeeGroup
-import org.bubblecloud.zigbee.v3.zcl.protocol.ZclClusterType
-import org.bubblecloud.zigbee.v3.zcl.protocol.command.general.ReportAttributesCommand
 import org.bubblecloud.zigbee.v3.zcl.protocol.command.ias.zone.ZoneStatusChangeNotificationCommand
 import kotlin.concurrent.fixedRateTimer
 
 fun main(args : Array<String>) {
-    println("Zikky startup...")
     val api = ZigBeeGatewayClient("http://127.0.0.1:5000/", "secret")
+
+    println("Zikky startup...")
     api.startup()
     println("Zikky startup.")
 
     onExit {
-        println()
         println("Zikky shutdown...")
         api.shutdown()
         println("Zikky shutdown.")
     }
 
-    var devices = api.zigBeeDevices.filter { it.label != null }.associateBy({it.label}, {it})
-    var lamps = ZigBeeGroup(0)
+    val devices = api.devices.filter { it.label != null }.associateBy({it.label}, {it})
+    val groups = api.groups.associateBy({it.label}, {it})
 
-    println()
-    println("Labeled devices in network:")
+    val zone = devices["zone"]!!
+    val lamps = groups["lamps"]!!
 
-    for ((index, device) in devices.values.withIndex()) {
-        val label: String
-        if (device.label != null) {
-            label = device.label
-        } else {
-            label = "<no label>"
-        }
-        print(index.toString().padStart(3))
-        print(") ")
-        print(device.networkAddress.toString().padEnd(6))
-        print(": ")
-        print(label)
-        println()
-    }
-
-    var occupied = false
+    var movement = false
     var lighting = false
-    var lastStateChangeTimeMillis = System.currentTimeMillis()
+    var changeTime = currentTime()
 
     api.addCommandListener {
         if (it is ZoneStatusChangeNotificationCommand) {
             val command: ZoneStatusChangeNotificationCommand = it
-            val alarm1Mask = 1 shl 0
-            val alarm2Mask = 1 shl 1
-            val alarm1 = command.zoneStatus and alarm1Mask > 0
-            val alarm2 = command.zoneStatus and alarm2Mask > 0
-            println("Zone status alarm 1: $alarm1 alarm 2: $alarm2")
 
-            occupied = alarm1 || alarm2
-            lastStateChangeTimeMillis = System.currentTimeMillis()
+            if (command.sourceAddress == zone.networkAddress && command.sourceEnpoint == zone.endpoint) {
+                val alarm1 = command.zoneStatus and (1 shl 0) > 0
+                val alarm2 = command.zoneStatus and (1 shl 1) > 0
+                movement = alarm1 || alarm2
+                changeTime = currentTime()
+                if (movement) {
+                    println("Zone movement detected.")
+                } else {
+                    println("Zone movement not detected.")
+                }
+            }
         }
     }
 
-    fixedRateTimer("light-control", true, 0, 1000, {
-        if (occupied != lighting) {
-            if (occupied) {
+    fixedRateTimer("Lighting timer", true, 0, 1000, {
+        if (movement != lighting) {
+            if (movement) {
                 api.on(lamps)
                 lighting = true
-                lastStateChangeTimeMillis = System.currentTimeMillis()
+                changeTime = currentTime()
                 println("Occupied, switched lamps on.")
             } else {
-                if (System.currentTimeMillis() - lastStateChangeTimeMillis > 15000) {
+                if (currentTime() - changeTime > 15 * 60000) {
                     api.off(lamps)
                     lighting = false
-                    lastStateChangeTimeMillis = System.currentTimeMillis()
+                    changeTime = currentTime()
                     println("Unoccupied, switched lamps off.")
-                } else {
-                    println("Unoccupied, waiting to switch lamps off.")
                 }
             }
         }
     })
 
+}
+
+fun currentTime(): Long {
+    return System.currentTimeMillis()
 }
 
 fun onExit(shutdownHook: () -> Unit) {
