b798ecdbd28add206a2728ee8697ebc07561162d
Tommi S.E. Laukkanen <tommi.s.e.laukkanen@gmail.com>
2016-07-05 11:25:13 +0300

    Implemented simple occupancy based light group on / off switching.

--- src/main/kotlin/org/bubblecloud/zikky/Zikky.kt
+++ src/main/kotlin/org/bubblecloud/zikky/Zikky.kt
@@ -1,6 +1,7 @@
 package org.bubblecloud.zikky
 
 import org.bubblecloud.zigbee.v3.ZigBeeGatewayClient
+import org.bubblecloud.zigbee.v3.ZigBeeGroup
 import org.bubblecloud.zigbee.v3.zcl.protocol.ZclClusterType
 import org.bubblecloud.zigbee.v3.zcl.protocol.command.general.ReportAttributesCommand
 import org.bubblecloud.zigbee.v3.zcl.protocol.command.ias.zone.ZoneStatusChangeNotificationCommand
@@ -11,14 +12,15 @@ fun main(args : Array<String>) {
     api.startup()
     println("Zikky startup.")
 
-    onExit({
+    onExit {
         println()
         println("Zikky shutdown...")
         api.shutdown()
         println("Zikky shutdown.")
-    })
+    }
 
     var devices = api.zigBeeDevices.filter { it.label != null }.associateBy({it.label}, {it})
+    var lamps = ZigBeeGroup(0)
 
     println()
     println("Labeled devices in network:")
@@ -47,30 +49,27 @@ fun main(args : Array<String>) {
             val alarm2 = command.zoneStatus and alarm2Mask > 0
             println("Zone status alarm 1: $alarm1 alarm 2: $alarm2")
         }
+
         if (it is ReportAttributesCommand) {
             val command: ReportAttributesCommand = it
             if (command.clusterId == ZclClusterType.OCCUPANCY_SENSING.id) {
                 for (report in command.reports) {
                     println("Occupancy and sensing reports: ${report.attributeIdentifier} = ${report.attributeValue}")
+
+                    if (report.attributeIdentifier == 0) {
+                        if (report.attributeValue == 1) {
+                            api.on(lamps)
+                        } else {
+                            api.off(lamps)
+                        }
+                    }
+
                 }
             }
 
         }
     }
 
-    Thread.sleep(10000)
-
-    val lampOne = devices["l1"]
-
-    if (lampOne != null) {
-        api.off(lampOne)
-    }
-
-    exit()
-}
-
-fun exit() {
-    Runtime.getRuntime().exit(0)
 }
 
 fun onExit(shutdownHook: () -> Unit) {
